d20ddbbd118f2da843692091f7c491e41fc2bd57
jagat-developer <jagat@codepaper.com>
2026-05-27 10:35:11 -0400

    gtm

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ const siteUrl = "https://hrgreenrootslandscaping.com/";
 const defaultDescription =
   "HR Greenroots Landscaping provides landscaping, hardscaping, sod installation, interlocking, deck, fence, and garden design services across Mississauga and the GTA.";
 const googleAnalyticsId = "G-ZDQJHLKZ0Q";
+const googleTagManagerId = "GTM-KJDG3Q2K";
 
 export const metadata = {
   metadataBase: new URL(siteUrl),
@@ -48,7 +49,28 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${googleTagManagerId}');
+            `,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
           strategy="afterInteractive"
